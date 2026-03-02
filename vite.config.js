60933964f946e070910ef924c86e2a979ea4c91e
MahalakshmiAK <99109631+MahalakshmiAK@users.noreply.github.com>
2026-03-02 17:04:28 +0530

    Updating server configuration to vite.config.js
    Configure Vite server settings for external access.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows the dev server to be accessible from outside the container
+    port: 5173,
+    strictPort: true,
+  },
 })
